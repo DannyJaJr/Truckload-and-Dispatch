f1226a9744997fabed009fd5e1cf7fbd07148f70
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-01 14:12:32 -0400

    add-load.html

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -4,5 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('my-loads', user_loads, name="my-loads")
+    path('my-loads', user_loads, name="my-loads"),
+    path('add-load', AddLoad.as_view(), name="add-load"),
 ]
+
+
+
