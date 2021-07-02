8bd95df72e6183f2745ab4b1f233e2141475edfb
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-02 16:35:40 -0400

    add

--- truckload/settings.py
+++ truckload/settings.py
@@ -28,6 +28,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
