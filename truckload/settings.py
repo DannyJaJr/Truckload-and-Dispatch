87093354eccd08481349c9ef6dbe2969c86da252
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-02 17:41:09 -0400

    added pips

--- truckload/settings.py
+++ truckload/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,7 +154,9 @@ LOGIN_URL = "/auth/login"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 """
 Proficel
