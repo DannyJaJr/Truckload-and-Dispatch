1d49acb36f72c754da8d45b98eca391a3ae9aaf8
Daniel Lafontant <ldanieljajr@gmail.com>
2021-07-02 16:00:39 -0400

    deploy prob

--- truckload/settings.py
+++ truckload/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 # to allow storage fro image on media file
-import os
+import os import environ
 from pathlib import Path
 # to install a .env to store secret keys => pip install python-decouple then from decouple import config
 from decouple import config
+
+
+print('(config)', environ)
+SECRET_KEY = environ['SECRET_KEY']
+print('(SECRET_KEY)', SECRET_KEY)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
